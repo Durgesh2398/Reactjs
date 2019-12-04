d6f44d80d24bd45e9ef5d27de950d9364c05f10f
Durgesh2398 <2308durgesh@gmail.com>
2019-12-04 10:45:23 +0530

    Dynamic Content: Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import React from 'react';
+// import React from 'react';
+// import Header from './Header.js';
+// import './App.css';
+
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Header/>
+//       <div className="component-body-container">
+//           <button className="custom-btn add-btn">Add</button>
+
+//           <div className="grid-container heading-container">
+//             <span className="grid-item name-heading">Name</span>
+//             <span className="grid-item phone-heading">Phone</span>
+//           </div>
+//         </div>
+//     </div>
+//     );
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react';
 import Header from './Header.js';
 import './App.css';
 
 
+class App extends Component {
+  render() {
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <div className="component-body-container">
+    let subscribers = [
+      {
+        id: 1,
+        name: "Durgesh",
+        phone: "9356196308"
+      },
+      {
+        id: 2,
+        name: "Durgesh Kumar",
+        phone: "9023628980"
+      }
+    ];
+
+    return (
+      <div>
+        <Header />
+        <div className="component-body-container">
           <button className="custom-btn add-btn">Add</button>
-
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
+
+
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+            })
+          }
         </div>
-    </div>
+      </div>
     );
+  }
 }
+
+
 
 export default App;
